76529f6a6a4ef196d43a7613b3eefab47fa3a6dd
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 15:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+// function toCase(text) {
+//   // write your code here
+// }
+
+// // DO not change the code below
+
+// const text = prompt("Enter text:");
+// alert(toCase(text));
+
+
 function toCase(text) {
-  // write your code here
+  // Check if the input string is empty
+  if (text.length === 0) {
+    return '-';
+  }
+
+  // Convert the string to lowercase and uppercase
+  const lowerCaseText = text.toLowerCase();
+  const upperCaseText = text.toUpperCase();
+
+  // Return the formatted string with - as the delimiter
+  return `${lowerCaseText}-${upperCaseText}`;
 }
 
-// DO not change the code below
-
+// Example
 const text = prompt("Enter text:");
 alert(toCase(text));
